67a7234c26abdaac20b05f27559660f9bfd8454a
Matthew Phillips <phillips.matthew829@gmail.com>
2022-02-02 12:16:09 -0600

    commit tree fa03b483ada7a14d6f77d265bc68cbb59038d539

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+/*
+BrowserRouter is wrapped around the topmost Component.
+Because of this, it is also wrapped around all of its 
+child Components too!
+*/
